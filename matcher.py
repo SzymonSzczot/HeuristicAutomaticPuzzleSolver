37da2e0ed9464b7721dbfff546fc18a775edf3b9
SzymonSzczot <38433235+SzymonSzczot@users.noreply.github.com>
2020-09-10 22:42:30 +0200

    User Interface

--- matcher.py
+++ matcher.py
@@ -1,19 +1,38 @@
 import cv2
 import image_slicer
 from matplotlib import pyplot as plt
-
-PATH_TO_BASE_IMAGE = "images/planet.jpg"
+import os
+# PATH_TO_BASE_IMAGE = "images/planet.jpg"
 NUMBER_OF_TILES = 30
 
 if __name__ == '__main__':
+    inp = input("If you want to use your own picture input path, else: 'N'")
+    if not (inp == "N" or inp == "n"):
+        path = inp
+        while not os.path.isfile(path):
+            path = input("Input patch to picture")
+        NUMBER_OF_TILES = int(input("How many tiles do you want?"))
+        tiles = image_slicer.slice(path, NUMBER_OF_TILES)
+        temp_path = path
+        images = [
+            tile.filename
+            for tile in tiles
+        ]
+    else:
+        path = ""
+        while not os.path.isdir(path):
+            path = input("Input patch to folder with puzzles")
+        temp_path = ""
+        while not os.path.isfile(temp_path):
+            temp_path = input("Path to picture")
 
-    tiles = image_slicer.slice(PATH_TO_BASE_IMAGE, NUMBER_OF_TILES)
-    images = [
-        tile.filename
-        for tile in tiles
-    ]
-    template = cv2.imread(PATH_TO_BASE_IMAGE, 0)
-    tem = cv2.imread(PATH_TO_BASE_IMAGE, 1)
+        images = [
+            path + "\\" + im
+            for im in os.listdir(path)
+        ]
+
+    template = cv2.imread(temp_path, 0)
+    tem = cv2.imread(temp_path, 1)
 
     results = []
     for index, i in enumerate(images):
@@ -29,6 +48,7 @@ if __name__ == '__main__':
         bottom_right = (top_left[0] + w, top_left[1] + h)
         i = i.split("\\")[1]
         i = i.replace(".png", "")
+        i = i.replace(".jpg", "")
 
         cv2.rectangle(tem, top_left, bottom_right, 255, 2)
         cv2.putText(
